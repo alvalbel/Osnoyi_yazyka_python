4a0934001fb07a57ba80749060fb08868005ebd5
alvalbel <belobraginav@gmail.ru>
2021-01-22 20:46:19 +0400

    домашнее задание к уроку 6

--- lesson_4_task_5.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-my_list = list(range(100, 1001))
-new_list = [el for el in my_list if el % 2 == 0]
-print(reduce(lambda x, y: x * y, new_list))
--- lesson_4_task_7.py
+++
@@ -1,7 +0,0 @@
-def fact(n):
-    n = int(4)
-    for el in fact(n):
-        math.fact(n)
-    yield fact(n)
-    print(fact(n))  # не пойму почему не принтит О_О
-
